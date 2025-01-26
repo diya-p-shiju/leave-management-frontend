cfc79c55cb176654b0d18c8948e2c621f71a6bc9
diya-p-shiju <diya.p.shiju@gmail.com>
2025-01-26 22:24:08 +0400

    all the types have been corrected

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,10 @@
 export interface User {
-    _id:number;
+    _id:string;
     name: string;
     email: string;
     password: string;
     role: string;
-    department: number;
+    department: string;
 }
 
 
